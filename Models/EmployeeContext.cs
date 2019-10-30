2ac791e77e70eeb12e89f27824e02e96a7dd6282
Mauricio Rojas Vindas <mrojas@impesa.net>
2019-10-30 16:28:58 -0600

    Base Sample

---
+++ Models/EmployeeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreCodeFirstSample.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
